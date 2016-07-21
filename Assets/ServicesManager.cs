7d825f4a3a9e6e7bd77ec92b240fe4d9b082ee6d
Raicuparta <raicuparta@gmail.com>
2016-07-21 01:21:22 +0100

    Services Manager for future refactoring

---
+++ Assets/ServicesManager.cs
@@ -0,0 +1,47 @@
+﻿using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using GooglePlayGames.BasicApi.Multiplayer;
+using UnityEngine;
+
+public class ServicesManager : MonoBehaviour {
+    bool AuthOnStart = true;
+    System.Action<bool> mAuthCallback;
+
+    void Start() {
+        mAuthCallback = (bool success) => {
+            // TODO Auth callback
+        };
+
+        PlayGamesClientConfiguration config =
+              new PlayGamesClientConfiguration.Builder()
+                .WithInvitationDelegate(OnGotInvitation)
+                .WithMatchDelegate(OnGotMatch)
+                .Build();
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.Activate();
+        PlayGamesPlatform.DebugLogEnabled = false;
+
+        // try silent authentication
+        if (AuthOnStart)
+            PlayGamesPlatform.Instance.Authenticate(mAuthCallback, true);
+    }
+
+    void OnEnable() {
+        // Request authentication
+        PlayGamesPlatform.Instance.localUser.Authenticate(mAuthCallback);
+    }
+
+    protected void OnGotInvitation(Invitation invitation, bool shouldAutoAccept) {
+        // Check if invite type is correct
+        if (invitation.InvitationType != Invitation.InvType.TurnBased) return;
+        // TODO
+    }
+
+    protected void OnGotMatch(TurnBasedMatch match, bool shouldAutoLaunch) {
+        // TODO
+    }
+
+    public void SetAuthOnStart(bool authOnStart) {
+        AuthOnStart = authOnStart;
+    }
+}
