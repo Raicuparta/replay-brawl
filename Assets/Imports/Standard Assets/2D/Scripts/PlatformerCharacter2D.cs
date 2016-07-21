59eb3c3881f7eaf8c7649b5c1281ef6f66a6cfe3
Raicuparta <raicuparta@gmail.com>
2016-07-21 20:55:02 +0100

    closes #7

--- Assets/Imports/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ Assets/Imports/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -4,59 +4,72 @@ using UnityEngine;
 namespace UnityStandardAssets._2D {
     public class PlatformerCharacter2D : MonoBehaviour {
         [SerializeField]
-        private float m_MaxSpeed = 10f;                    // The fastest the player can travel in the x axis.
+        private float MaxSpeed = 10f;                    // The fastest the player can travel in the x axis.
         [SerializeField]
-        private float m_JumpForce = 400f;                  // Amount of force added when the player jumps.
+        private float JumpForce = 400f;                  // Amount of force added when the player jumps.
         [SerializeField]
-        private bool m_AirControl = false;                 // Whether or not a player can steer while jumping;
+        private bool AirControl = false;                 // Whether or not a player can steer while jumping;
         [SerializeField]
-        private LayerMask m_WhatIsGround;                  // A mask determining what is ground to the character
+        private LayerMask WhatIsGround;                  // A mask determining what is ground to the character
+        [SerializeField]
+        private int AttackDuration = 10;                          // How many ticks the attack animation lasts
+        private int CurrentAttackTime = 0;
 
-        private Transform m_GroundCheck;    // A position marking where to check if the player is grounded.
+        private Transform GroundCheck;    // A position marking where to check if the player is grounded.
         const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
-        private bool m_Grounded;            // Whether or not the player is grounded.
-        private bool m_Attacking;
-        private Transform m_CeilingCheck;   // A position marking where to check for ceilings
+        private bool Grounded;            // Whether or not the player is grounded.
+        private bool Attacking;
+        private Transform CeilingCheck;   // A position marking where to check for ceilings
         const float k_CeilingRadius = .01f; // Radius of the overlap circle to determine if the player can stand up
-        private Rigidbody2D m_Rigidbody2D;
+        private Rigidbody2D Rigidbody2D;
 
         private void Awake() {
             // Setting up references.
-            m_GroundCheck = transform.Find("GroundCheck");
-            m_CeilingCheck = transform.Find("CeilingCheck");
-            m_Rigidbody2D = GetComponent<Rigidbody2D>();
+            GroundCheck = transform.Find("GroundCheck");
+            CeilingCheck = transform.Find("CeilingCheck");
+            Rigidbody2D = GetComponent<Rigidbody2D>();
         }
 
-
         private void FixedUpdate() {
-            m_Grounded = false;
+            Grounded = false;
             // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
             // This can be done using layers instead but Sample Assets will not overwrite your project settings.
-            Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(GroundCheck.position, k_GroundedRadius, WhatIsGround);
             for (int i = 0; i < colliders.Length; i++) {
                 if (colliders[i].gameObject != gameObject)
-                    m_Grounded = true;
+                    Grounded = true;
+            }
+
+            if (Attacking) Attack();
+        }
+
+        public void Attack() {
+            CurrentAttackTime++;
+            if (CurrentAttackTime > AttackDuration) {
+                Attacking = false;
+                CurrentAttackTime = 0;
+                GetComponent<Animator>().SetBool("Attack", false);
             }
         }
 
         public void Move(float move, bool attack, bool jump) {
             //only control the player if grounded or airControl is turned on
-            if (m_Grounded || m_AirControl) {
+            if (Grounded || AirControl) {
                 // Move the character
-                m_Rigidbody2D.velocity = new Vector2(move * m_MaxSpeed, m_Rigidbody2D.velocity.y);
+                Rigidbody2D.velocity = new Vector2(move * MaxSpeed, Rigidbody2D.velocity.y);
             }
 
             // If the player should jump...
-            if (m_Grounded && jump) {
+            if (Grounded && jump) {
                 // Add a vertical force to the player.
-                m_Grounded = false;
-                m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+                Grounded = false;
+                Rigidbody2D.AddForce(new Vector2(0f, JumpForce));
             }
 
             // If the player should attack...
             if (attack) {
-                Debug.Log("Attack");
-                m_Attacking = true;
+                Attacking = true;
+                GetComponent<Animator>().SetBool("Attack", true);
             }
         }
     }
