76a6ab3f62809f7e853d42a18d165efc2bbbc39d
Raicuparta <raicuparta@gmail.com>
2016-07-21 02:44:13 +0100

    new menu scene, to separate from the main game scene

--- Assets/ServicesManager.cs
+++ Assets/ServicesManager.cs
@@ -2,14 +2,32 @@
 using GooglePlayGames.BasicApi;
 using GooglePlayGames.BasicApi.Multiplayer;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ServicesManager : MonoBehaviour {
     bool AuthOnStart = true;
-    System.Action<bool> mAuthCallback;
+    System.Action<bool> AuthCallback;
+
+    // Always 1v1 match so only 1 opponent for now
+    private const int Opponents = 1;
+    // Default matching - matches everything as 0xffffffff
+    private const uint Variant = 0xffffffff;
 
     void Start() {
-        mAuthCallback = (bool success) => {
-            // TODO Auth callback
+        AuthCallback = (bool success) => {
+            if (success) {
+                Debug.Log("Login Success");
+                // Activate menu buttons that require login
+                foreach (Transform child in transform) {
+                    Button button = child.GetComponent<Button>();
+                    if (child.name == "Login") {
+                        // Show user name instead of Login button
+                        button.GetComponentInChildren<Text>().text = PlayGamesPlatform.Instance.GetUserDisplayName();
+                        button.interactable = false;
+                    } else
+                        button.interactable = true;
+                }
+            } else Debug.LogError("Error logging in");
         };
 
         PlayGamesClientConfiguration config =
@@ -23,12 +41,47 @@ public class ServicesManager : MonoBehaviour {
 
         // try silent authentication
         if (AuthOnStart)
-            PlayGamesPlatform.Instance.Authenticate(mAuthCallback, true);
+            PlayGamesPlatform.Instance.Authenticate(AuthCallback, true);
     }
 
-    void OnEnable() {
+    public void Login() {
         // Request authentication
-        PlayGamesPlatform.Instance.localUser.Authenticate(mAuthCallback);
+        PlayGamesPlatform.Instance.localUser.Authenticate(AuthCallback);
+    }
+
+    public void Logout() {
+        PlayGamesPlatform.Instance.SignOut();
+        foreach (Transform child in transform) {
+            Button button = child.GetComponent<Button>();
+            if (child.name == "Login") {
+                // Change the login button back to its original state
+                button.GetComponentInChildren<Text>().text = "Login";
+                button.interactable = true;
+            } else
+                button.interactable = false;
+        }
+        AuthOnStart = false;
+    }
+
+    public void Invite() {
+        PlayGamesPlatform.Instance.TurnBased.CreateWithInvitationScreen(Opponents, Opponents, Variant, OnMatchStarted);
+    }
+
+    public void Random() {
+        PlayGamesPlatform.Instance.TurnBased.CreateQuickMatch(Opponents, Opponents, Variant, OnMatchStarted);
+    }
+
+    public void Inbox() {
+        PlayGamesPlatform.Instance.TurnBased.AcceptFromInbox(OnMatchStarted);
+    }
+
+    protected void OnMatchStarted(bool success, TurnBasedMatch match) {
+        if (!success) {
+            Debug.Log("Error starting match");
+            return;
+        }
+
+        // TODO
     }
 
     protected void OnGotInvitation(Invitation invitation, bool shouldAutoAccept) {
@@ -39,9 +92,5 @@ public class ServicesManager : MonoBehaviour {
 
     protected void OnGotMatch(TurnBasedMatch match, bool shouldAutoLaunch) {
         // TODO
-    }
-
-    public void SetAuthOnStart(bool authOnStart) {
-        AuthOnStart = authOnStart;
     }
 }
