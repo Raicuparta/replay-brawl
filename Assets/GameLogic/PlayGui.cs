ea60017bb546308bd1714e8529b1c75d44269a08
Raicuparta <raicuparta@gmail.com>
2016-07-22 06:53:53 +0100

    removed some useless Unity GUI references

--- Assets/GameLogic/PlayGui.cs
+++ Assets/GameLogic/PlayGui.cs
@@ -1,62 +1,17 @@
-﻿/*
- * Copyright (C) 2014 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-using UnityEngine;
-using System.Collections;
+﻿using UnityEngine;
 using GooglePlayGames;
 using GooglePlayGames.BasicApi.Multiplayer;
-using System.Collections.Generic;
 
-public class PlayGui : BaseGui {
-    public GameObject Playfield;
+public class PlayGui : MonoBehaviour {
     public Capture Player;
     public Capture Opponent;
-
-    private const string BlocksRootName = "BlocksRoot";
-    private const string XBlockPrefabName = "XBlock";
-    private const string OBlockPrefabName = "OBlock";
-    private const string LargeMarkPrefix = "LargeMark";  // + "X00", "X01", "O00", etc
-
-    WidgetConfig DoneCfg = new WidgetConfig(-0.2f, -0.2f, 0.2f, 0.1f, 60, "Done");
-    WidgetConfig OkButtonCfg = new WidgetConfig(0.0f, 0.4f, 0.4f, 0.2f, 60, "OK");
 
     private TurnBasedMatch mMatch = null;
     private MatchData mMatchData = null;
     private string mFinalMessage = null;
     private char mMyMark = '\0';
 
-    // has a block been shot and is in flight?
-    private bool mBlockInFlight = false;
-
-    // the countdown to check if all blocks are at rest
-    private float mRestCheckCountdown = 0.0f;
-    private const float RestCheckInterval = 1.0f;
-
-    // maximum time to wait for all blocks to come to rest
-    private const float MaxTurnTime = 8.0f;
-    private float mEndTurnCountdown = MaxTurnTime;
-
-    // tile size (for calculating which large mark to attribute to each box)
-    private const float TileSize = 1.3f;
-
     private bool mEndingTurn = false;
-
-    // mass of the player's block (it's more massive than other blocks to
-    // make for more interesting gameplay)
-    private const float PlayerBlockMass = 10.0f;
 
     // countdown to hide instructions
     private bool mShowInstructions = false;
@@ -70,11 +25,8 @@ public class PlayGui : BaseGui {
         mMatchData = null;
         mFinalMessage = null;
         mMyMark = '\0';
-        mEndTurnCountdown = MaxTurnTime;
-        mBlockInFlight = false;
         mEndingTurn = false;
         mShowInstructions = false;
-        Util.MakeVisible(Playfield, false);
         Opponent.gameObject.SetActive(true);
         Opponent.Reset();
         //Player.Reset();
@@ -83,7 +35,6 @@ public class PlayGui : BaseGui {
     public void LaunchMatch(TurnBasedMatch match) {
         Reset();
         mMatch = match;
-        MakeActive();
 
         if (mMatch == null) {
             throw new System.Exception("PlayGui can't be started without a match!");
@@ -149,42 +100,7 @@ public class PlayGui : BaseGui {
         return "Error";
     }
 
-    protected override void DoGUI() {
-        if (mFinalMessage != null) {
-            GuiLabel(CenterLabelCfg, mFinalMessage);
-            if (GuiButton(OkButtonCfg)) {
-                Reset();
-                gameObject.GetComponent<MainMenuGui>().MakeActive();
-            }
-            return;
-        }
-
-        if (GuiButton(DoneCfg)) {
-            SetStandBy("Sending...");
-            EndTurn();
-        }
-    }
-
-    private static GameObject Spawn(GameObject parent, string prefabName) {
-        GameObject o = (GameObject) GameObject.Instantiate(Resources.Load(prefabName));
-        if (parent != null) {
-            o.transform.parent = parent.transform;
-        }
-        return o;
-    }
-
-    private static GameObject Spawn(GameObject parent, string prefabName, Vector3 position, Quaternion rotation) {
-        GameObject o = (GameObject) GameObject.Instantiate(Resources.Load(prefabName), position, rotation);
-        if (parent != null) {
-            o.transform.parent = parent.transform;
-        }
-        return o;
-    }
-
     private void SetupObjects(bool canPlay) {
-        // show the play field
-        Util.MakeVisible(Playfield, true);
-
         string replay = mMatchData.Replay;
         if (replay != null) {
             Opponent.ReadFromString(mMatchData.Replay);
@@ -192,52 +108,6 @@ public class PlayGui : BaseGui {
         }
         else {
             Opponent.gameObject.SetActive(false);
-        }
-
-        // create the blocks
-        /*foreach (MatchData.BlockDesc desc in mMatchData.BlockDescs) {
-            Spawn(GetBlocksRoot(), desc.mark == MatchData.MarkX ?
-                XBlockPrefabName : OBlockPrefabName, desc.position, desc.rotation);
-        }*/
-
-        // create the block the player is shooting, if applicable
-        if (canPlay) {
-            // the block prefabs get instantiated in the right
-            // position for this, so we don't need to translate
-            /*GameObject o = Spawn(GetBlocksRoot(), mMyMark == MatchData.MarkX ?
-                    XBlockPrefabName : OBlockPrefabName);
-            o.AddComponent<AimController>().SetFireDelegate(OnBlockFired);
-            o.AddComponent<CollisionSfx>();
-            o.GetComponent<Rigidbody>().mass = PlayerBlockMass;*/
-        }
-    }
-
-    // notifies us that a block was fired (and is currently in flight)
-    void OnBlockFired() {
-        mBlockInFlight = true;
-        mShowInstructions = false;
-    }
-
-    void Update() {
-        if (mEndingTurn) {
-            return;
-        }
-
-        mRestCheckCountdown -= Time.deltaTime;
-
-        if (mBlockInFlight) {
-            mEndTurnCountdown -= Time.deltaTime;
-
-            // are all the blocks at rest?
-            /*if (mRestCheckCountdown < 0) {
-                if (AllBlocksAtRest() || mEndTurnCountdown < 0) {
-                    // all blocks are at rest -- we are ready to take the turn
-                    EndTurn();
-                } else {
-                    // check again momentarily
-                    mRestCheckCountdown = RestCheckInterval;
-                }
-            }*/
         }
     }
 
@@ -254,13 +124,6 @@ public class PlayGui : BaseGui {
 
     void EndTurn() {
         mEndingTurn = true;
-
-        // save current state of the blocks into our match data
-        /*mMatchData.ClearBlockDescs();
-        foreach (GameObject o in GetAllGameBlocks()) {
-            char mark = o.tag == "MarkX" ? MatchData.MarkX : MatchData.MarkO;
-            mMatchData.AddBlockDesc(mark, o.transform.position, o.transform.rotation);
-        }*/
 
         // do we have a winner?
         if (mMatchData.HasWinner) {
@@ -291,22 +154,22 @@ public class PlayGui : BaseGui {
             winnerIsMe ? MatchOutcome.ParticipantResult.Loss : MatchOutcome.ParticipantResult.Win);
 
         // finish the match
-        SetStandBy("Sending...");
+        //SetStandBy("Sending...");
         PlayGamesPlatform.Instance.TurnBased.Finish(mMatch, mMatchData.ToBytes(),
                     outcome, (bool success) => {
-            EndStandBy();
+            //EndStandBy();
             mFinalMessage = success ? (winnerIsMe ? "YOU WON!" : "YOU LOST!") :
                 "ERROR finishing match.";
         });
     }
 
     void TakeTurn() {
-        SetStandBy("Sending...");
+        //SetStandBy("Sending...");
         mMatchData.Replay = Player.ToString();
 
         PlayGamesPlatform.Instance.TurnBased.TakeTurn(mMatch, mMatchData.ToBytes(),
                     DecideNextToPlay(), (bool success) => {
-            EndStandBy();
+            //EndStandBy();
             mFinalMessage = success ? "Done for now!" : "ERROR sending turn.";
         });
     }
