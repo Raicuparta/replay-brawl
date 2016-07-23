9d351218b796ecd71b0f58e23be462a2d135ed23
Raicuparta <raicuparta@gmail.com>
2016-07-23 12:58:37 +0100

    closes #12

--- Assets/Behaviors/HealthManager.cs
+++ Assets/Behaviors/HealthManager.cs
@@ -9,13 +9,17 @@ public class HealthManager : MonoBehaviour {
     [SerializeField]
     int HealthDecrement = 10; // Amount of health to remove when receiving an attack
     ParticleSystem Particles;
+    Transform HealthBar;
+    float InitialHealthScaleX;
 
     void Awake() {
         Particles = GetComponent<ParticleSystem>();
+        HealthBar = transform.Find("Health");
     }
 
     void Start() {
         Health = InitialHealth;
+        InitialHealthScaleX = HealthBar.localScale.x;
     }
 
     public void Hit() {
@@ -26,12 +30,19 @@ public class HealthManager : MonoBehaviour {
 
         // Decrease health
         Health -= HealthDecrement;
+        UpdateHealthBar();
 
         // Kill if no leath left
         if (Health <= 0) Death();
     }
 
-    private void Death() {
+    void UpdateHealthBar() {
+        Vector2 newScale = HealthBar.localScale;
+        newScale.x = ((float)Health / InitialHealth) * InitialHealthScaleX;
+        HealthBar.localScale = newScale;
+    }
+
+    void Death() {
         Dead = true;
         Debug.Log("Someone died");
     }
