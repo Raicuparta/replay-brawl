c8d60f392588db36d01e6378ff2e6b04facac1a4
Raicuparta <raicuparta@gmail.com>
2016-07-27 00:23:22 +0100

    closes #47, also shake screen on attack

--- Assets/Behaviors/Attack.cs
+++ Assets/Behaviors/Attack.cs
@@ -9,6 +9,14 @@ public class Attack : MonoBehaviour {
     bool Attacking;
     Rigidbody2D Body;
 
+    [SerializeField]
+    float AttackPauseTime = 0.1f;  // How long to freeze the game for when attack connects
+    [SerializeField]
+    float ShakeAplitude = 0.1f;    // How much to shake the screen when an attack connects
+    [SerializeField]
+    float ShakeTime = 0.2f;
+    public Camera MainCamera;
+
     private void Start() {
         Body = GetComponent<Rigidbody2D>();
     }
@@ -25,10 +33,16 @@ public class Attack : MonoBehaviour {
         //Debug.Log("Detect Atack1 " + collider.tag);
         // Register hit
         if (Attacking && collider.tag == "Attackable") {
-            Debug.Log("Detect Atack " + collider.name);
-            Attacking = false;
-            collider.GetComponent<HealthManager>().Hit();
+            ConnectAttack(collider);
         }
+    }
+
+    void ConnectAttack(Collider2D collider) {
+        Debug.Log("Detect Atack " + collider.name);
+        Attacking = false;
+        collider.GetComponent<HealthManager>().Hit();
+        StartCoroutine(PauseFor(AttackPauseTime));
+        ShakeScreen();
     }
 
     public void UpdateAttack() {
@@ -52,5 +66,34 @@ public class Attack : MonoBehaviour {
             Attacking = true;
             GetComponent<Animator>().SetBool("Attack", true);
         }
+    }
+
+    private IEnumerator PauseFor(float time) {
+        Time.timeScale = 0f;
+        float pauseEndTime = Time.realtimeSinceStartup + time;
+        while (Time.realtimeSinceStartup < pauseEndTime) {
+            yield return 0;
+        }
+        Time.timeScale = 1;
+    }
+
+    void ShakeScreen() {
+        InvokeRepeating("StartShaking", 0, .01f);
+        Invoke("StopShaking", ShakeTime);
+    }
+
+    void StartShaking() {
+        if (ShakeAplitude > 0) {
+            float shakeX = Random.value * ShakeAplitude * 2 - ShakeAplitude;
+            float shakeY = Random.value * ShakeAplitude * 2 - ShakeAplitude;
+            Vector3 pp = MainCamera.transform.position;
+            pp.x += shakeX;
+            pp.y += shakeY;
+            MainCamera.transform.position = pp;
+        }
+    }
+
+    void StopShaking() {
+        CancelInvoke("StartShaking");
     }
 }
